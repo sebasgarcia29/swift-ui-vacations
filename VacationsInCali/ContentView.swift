05f52d305d0ad9b60e31b049f6fb701529e3dd57
Sebastian <sebastiang@takeoffmedia.com>
2024-10-16 17:06:21 -0500

    Initial Commit

---
+++ VacationsInCali/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VacationsInCali
+//
+//  Created by Sebastian Garcia on 16/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VacationsInCali/VacationsInCaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationsInCaliApp.swift
+//  VacationsInCali
+//
+//  Created by Sebastian Garcia on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationsInCaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
